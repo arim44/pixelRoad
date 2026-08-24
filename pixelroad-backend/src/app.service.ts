d86520509f01de3473de67fb9d20cc534b0b32d0
arim44 <hongarim44@gmail.com>
2026-08-24 22:57:09 +0900

    feat:2nd 환경변수 추가

--- pixelroad-backend/src/app.service.ts
+++ pixelroad-backend/src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    const hfToken = process.env.HF_TOKEN ?? '(미설정)';
+    const hfModel = process.env.HF_MODEL ?? '(미설정)';
+
+    return {
+      message: 'Hello World! -app service',
+      HF_TOKEN: hfToken,
+      HF_MODEL: hfModel,
+    };
   }
 }
